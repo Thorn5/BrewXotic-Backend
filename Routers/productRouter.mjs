0ed62c0da3aa8102273238ea30ba0da67cc8f221
Trygve Horn <trygvehorn@gmail.com>
2023-04-08 11:44:53 +0200

    Converted to try, added async await

--- Routers/productRouter.mjs
+++ Routers/productRouter.mjs
@@ -3,6 +3,21 @@ const router = express.Router();
 import Product from '../Models/ProductSchema.mjs';
 
 const collection =
+    //* original version
+    // router.get('/', (req, res) => {
+    //     Product.find({})
+    //       .then(data => {
+    //         if (!data) {
+    //           return res.sendStatus(404); 
+    //         }
+    //         res.json(data);
+    //       })
+    //       .catch(err => {
+    //         console.log(err.message);
+    //         res.sendStatus(500);
+    //       });
+    //     });
+
     router.get('/', async (req, res) => {
         try {
             const data = await Product.find({});
@@ -15,6 +30,21 @@ const collection =
             res.sendStatus(500);
         }
     });
+//* original version
+// router.get('/:id', (req, res) => {
+//     const { id } = req.params;
+//     Product.findById(id)
+//     .then(data => {
+//       if (!data) {
+//           return res.sendStatus(404); 
+//         }
+//         res.json(data);
+//       })
+//       .catch(err => {
+//         console.log(err.message);
+//         res.sendStatus(500);
+//       });
+//     });
 
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
