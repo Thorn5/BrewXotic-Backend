ec568d613f417288bfb57c9b6668e7c560bee891
Trygve Horn <trygvehorn@gmail.com>
2023-04-21 12:46:58 +0200

    modified order schema

--- Models/OrderSchema.mjs
+++ Models/OrderSchema.mjs
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const OrderSchema = new mongoose.Schema({
-    customer_id: { type: mongoose.Schema.Types.ObjectId, required: true },
-    items: [{
+  customer_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+  items: [{
       product_id: { type: mongoose.Schema.Types.ObjectId, required: true },
-      quantity: { type: Number, required: true }
-    }]
-  }, { timestamps: true });
+      quantity: { type: Number, required: true },
+      price: { type: Number, required: true },
+  }]
+},
+  { timestamps: true });
   
 
 const Order = mongoose.model("Order", OrderSchema);
